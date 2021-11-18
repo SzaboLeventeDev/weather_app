a61b6a5b53dfe837fa3b8da57e0c045365919f20
Szabó Levente <szabo.levente.dev@gmail.com>
2021-11-18 21:15:09 +0100

    API works on currentDataContainer
    actualDataContainer works, actualDayInfo added in this section, hasn't tried with real data.

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
-/* const data = null;
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function(){
-    if (this.readyState === this.DONE) {
-        console.log(this.responseText);
-    }
-});
-
-xhr.open("GET", "https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=38.5&lat=-78.5");
-xhr.setRequestHeader("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com");
-xhr.setRequestHeader("x-rapidapi-key", "2dfa2f19c3mshef9b7bf2be454b1p160f2cjsnfbbbd48476e0");
-
-xhr.send(data); */
 var actualCityHeader = document.querySelector("#city");
+var actualTemperature = document.querySelector("#actualTemperature")
+var uvValue = document.querySelector("#uvValue");
+var windSpeed = document.querySelector("#windSpeed");
+var windDirection = document.querySelector("#windDirection")
+var sunRiseTime = document.querySelector("#sunRiseTime");
+var sunSetTime = document.querySelector("#sunSetTime");
+
 function weatherOfBudapest(){
-    const data = null;
-
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-        /* My lines under */
-        var obj = JSON.parse(xhr.responseText).count;
-        console.log(obj);
-        actualCityHeader.textContent = obj;
+    fetch("https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=19&lat=47.5", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
+		"x-rapidapi-key": "2dfa2f19c3mshef9b7bf2be454b1p160f2cjsnfbbbd48476e0"
 	}
-    
+})
+.then(response => {
+	console.log(response);
+    return response.json();
+})
+.then(function(actualData){
+    var appData = actualData.data[0];
+    console.log(actualData);
+    console.log(actualData.count)
+    console.log(actualData.data[0].city_name)
+    actualCityHeader.innerHTML = actualData.data[0].city_name;
+    actualTemperature.innerHTML = actualData.data[0].temp + " Celsius";
+    uvValue.innerHTML = actualData.data[0].uv;
+    windSpeed.innerHTML = actualData.data[0].wind_spd;
+    windDirection.innerHTML = actualData.data[0].wind_cdir_full;
+    sunRiseTime.innerHTML = actualData.data[0].sunrise;
+    sunSetTime.innerHTML = actualData.data[0].sunset;
+
+})
+.catch(err => {
+	console.error(err);
 });
-
-/* actualCityHeader.textContent = city; */
-
-
-xhr.open("GET", "https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=19&lat=47.5");
-xhr.setRequestHeader("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com");
-xhr.setRequestHeader("x-rapidapi-key", "2dfa2f19c3mshef9b7bf2be454b1p160f2cjsnfbbbd48476e0");
-
-xhr.send(data);
 }
+//Nem adja át az értékeket.
+/* function hourly (){
+
+}
+function actualDayInfo(actual){
+    var uvValue = document.querySelector("#uvValue");
+    uvValue.innerHTML = actual.data[0].uv;
+} */
 
 /* weatherOfBudapest(); */
